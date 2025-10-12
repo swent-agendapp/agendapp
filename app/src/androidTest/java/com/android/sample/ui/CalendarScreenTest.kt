78058b508537e39eb165f2027caf6989acf0813f
nillem-14 <meline@rmbi.ch>
2025-10-12 20:08:37 +0200

    test: add more tests

--- app/src/androidTest/java/com/android/sample/ui/CalendarScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/ui/CalendarScreenTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import com.android.sample.Agendapp
 import com.android.sample.ui.calendar.CalendarScreen
 import com.android.sample.ui.calendar.CalendarScreenTestTags
 import org.junit.Rule
@@ -27,4 +28,13 @@ class CalendarScreenTest {
 
     composeTestRule.onNodeWithTag(CalendarScreenTestTags.TOP_BAR_TITLE).assertTextEquals("Calendar")
   }
+  @Test
+  fun agendappCorrectlySet(){
+    composeTestRule.setContent { Agendapp() }
+
+    composeTestRule.onNodeWithTag(CalendarScreenTestTags.TOP_BAR_TITLE).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(CalendarScreenTestTags.TOP_BAR_TITLE).assertTextEquals("Calendar")
+  }
+
+
 }

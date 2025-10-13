ac5b5b4522e1a846dfd8ac59c4c13fcf1b0afdcb
Haobin Wang <haobin.wang@epfl.ch>
2025-10-13 17:41:16 +0200

    chore(format): fix ktfmt formatting issues

--- app/src/main/java/com/android/sample/ui/navigation/Navigation.kt
+++ app/src/main/java/com/android/sample/ui/navigation/Navigation.kt
@@ -4,46 +4,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import androidx.navigation.NavType
-import com.android.sample.ui.screens.HomeScreen
 import com.android.sample.ui.screens.EditEventScreen
+import com.android.sample.ui.screens.HomeScreen
 
 sealed class Screen(val route: String) {
-    data object Home : Screen("home")
-    data object EditEvent : Screen("edit_event/{eventId}") {
-        fun createRoute(eventId: String) = "edit_event/$eventId"
-    }
+  data object Home : Screen("home")
+
+  data object EditEvent : Screen("edit_event/{eventId}") {
+    fun createRoute(eventId: String) = "edit_event/$eventId"
+  }
 }
 
 @Composable
 fun AppNavigation(navController: NavHostController, modifier: Modifier = Modifier) {
-    NavHost(
-        navController = navController,
-        startDestination = Screen.Home.route,
-        modifier = modifier
-    ) {
+  NavHost(
+      navController = navController, startDestination = Screen.Home.route, modifier = modifier) {
         composable(Screen.Home.route) {
-            HomeScreen(onNavigateToEdit = {
-                navController.navigate(Screen.EditEvent.createRoute("E001"))
-            })
+          HomeScreen(
+              onNavigateToEdit = { navController.navigate(Screen.EditEvent.createRoute("E001")) })
         }
         composable(
             route = Screen.EditEvent.route,
-            arguments = listOf(navArgument("eventId") { type = NavType.StringType })
-        ) {
-            val eventId = it.arguments?.getString("eventId")
-            EditEventScreen(eventId = eventId, onNavigateBack = { navController.popBackStack() })
-        }
-    }
+            arguments = listOf(navArgument("eventId") { type = NavType.StringType })) {
+              val eventId = it.arguments?.getString("eventId")
+              EditEventScreen(eventId = eventId, onNavigateBack = { navController.popBackStack() })
+            }
+      }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun AppNavigationPreview() {
-    val navController = rememberNavController()
-    AppNavigation(navController)
+  val navController = rememberNavController()
+  AppNavigation(navController)
 }

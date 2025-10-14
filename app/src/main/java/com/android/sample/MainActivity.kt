c24dd25b7a64dd10e2576e1e2af83e8fd2b050f1
Haobin Wang <haobin.wang@epfl.ch>
2025-10-15 00:40:54 +0200

    refactor(navigation): move NavHost setup into MainActivity and use NavigationActions
    - Simplified architecture to keep only MainActivity and NavigationActions
    - MainActivity now handles NavController and NavHost initialization
    - NavigationActions encapsulates navigation logic

--- app/src/main/java/com/android/sample/MainActivity.kt
+++ app/src/main/java/com/android/sample/MainActivity.kt
@@ -10,27 +10,49 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.android.sample.resources.C
-import com.android.sample.ui.calendar.CalendarScreen
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.screens.EditEventScreen
+import com.android.sample.ui.screens.HomeScreen
 import com.android.sample.ui.theme.SampleAppTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      SampleAppTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-            modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
-            color = MaterialTheme.colorScheme.background) {
-              Agendapp()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SampleAppTheme {
+                val navController = rememberNavController()
+                val navigationActions = NavigationActions(navController)
+
+                Surface(
+                    modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation(navigationActions)
+                }
             }
-      }
+        }
     }
-  }
 }
 
 @Composable
-fun Agendapp() {
-  CalendarScreen()
+fun AppNavigation(navigationActions: NavigationActions) {
+    val navController = navigationActions.navController
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(onNavigateToEdit = { navigationActions.navigateToEdit("E001") })
+        }
+        composable(
+            route = "edit_event/{eventId}",
+            arguments = listOf(navArgument("eventId") { type = NavType.StringType })
+        ) {
+            val eventId = it.arguments?.getString("eventId") ?: ""
+            EditEventScreen(eventId = eventId, onNavigateBack = { navigationActions.navigateBack() })
+        }
+    }
 }
--- app/src/main/java/com/android/sample/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/android/sample/ui/navigation/NavigationActions.kt
@@ -1,45 +1,14 @@
 package com.android.sample.ui.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.android.sample.ui.screens.EditEventScreen
-import com.android.sample.ui.screens.HomeScreen
 
-sealed class Screen(val route: String) {
-  data object Home : Screen("home")
+class NavigationActions(val navController: NavHostController) {
 
-  data object EditEvent : Screen("edit_event/{eventId}") {
-    fun createRoute(eventId: String) = "edit_event/$eventId"
-  }
-}
+    fun navigateToEdit(eventId: String) {
+        navController.navigate("edit_event/$eventId")
+    }
 
-@Composable
-fun AppNavigation(navController: NavHostController, modifier: Modifier = Modifier) {
-  NavHost(
-      navController = navController, startDestination = Screen.Home.route, modifier = modifier) {
-        composable(Screen.Home.route) {
-          HomeScreen(
-              onNavigateToEdit = { navController.navigate(Screen.EditEvent.createRoute("E001")) })
-        }
-        composable(
-            route = Screen.EditEvent.route,
-            arguments = listOf(navArgument("eventId") { type = NavType.StringType })) {
-              val eventId = it.arguments!!.getString("eventId")!!
-              EditEventScreen(eventId = eventId, onNavigateBack = { navController.popBackStack() })
-            }
-      }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AppNavigationPreview() {
-  val navController = rememberNavController()
-  AppNavigation(navController)
+    fun navigateBack() {
+        navController.popBackStack()
+    }
 }

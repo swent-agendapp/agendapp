5d6116d7039fdc2b0e15e4d207d54f0611b0da1d
Qookie <nathan.duchosal@epfl.ch>
2025-10-14 22:54:21 +0200

    feat: add bootcamp authentification

---
+++ app/src/main/java/com/android/sample/model/authentification/User.kt
@@ -0,0 +1,14 @@
+package com.android.sample.model.authentification
+
+/**
+ * Data class representing a calendar event.
+ *
+ * @property id Unique identifier for the User.
+ * @property displayName Name to display for User Profile.
+ * @property email Email address of the User.
+ */
+data class User(
+    val id: String,
+    val displayName: String?,
+    val email: String?,
+)

88507a9200dbd79c717d64a42648e7e9843dc49e
N4dZ <dingweifeng.dw@gmail.com>
2025-10-16 15:47:08 +0200

    feat: add Material3 dependencies for newer UI components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -147,6 +147,10 @@ dependencies {
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.graphics)
     // Material Design 3
+    implementation("androidx.compose.material3:material3:1.3.1")
+
+    implementation("androidx.compose.material:material-icons-core:1.5.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0")
     implementation(libs.compose.material3)
     // Integration with activities
     implementation(libs.compose.activity)

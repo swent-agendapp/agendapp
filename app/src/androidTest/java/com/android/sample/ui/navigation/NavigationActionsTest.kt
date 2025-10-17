f114018b4857fadfb959ec87fb71dfbf77b16e5e
Qookie <nathan.duchosal@epfl.ch>
2025-10-17 07:24:10 +0200

    fix: test add input text to press next button

--- app/src/androidTest/java/com/android/sample/ui/navigation/NavigationActionsTest.kt
+++ app/src/androidTest/java/com/android/sample/ui/navigation/NavigationActionsTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.android.sample.AgendappNavigation
@@ -30,6 +31,8 @@ class AgendappNavigationTest {
     // Go to Calendar
     composeTestRule.onNodeWithTag(ADD_EVENT_BUTTON).assertExists().performClick()
     // Validate screen content
+    composeTestRule.onNodeWithTag(AddEventTestTags.TITLE_TEXT_FIELD).assertExists().performTextInput("Test Event")
+    composeTestRule.onNodeWithTag(AddEventTestTags.DESCRIPTION_TEXT_FIELD).assertExists().performTextInput("Test Description")
     composeTestRule.onNodeWithTag(AddEventTestTags.NEXT_BUTTON).assertExists().performClick()
     composeTestRule.onNodeWithTag(AddEventTestTags.NEXT_BUTTON).assertExists().performClick()
     composeTestRule.onNodeWithTag(AddEventTestTags.CREATE_BUTTON).assertExists().performClick()

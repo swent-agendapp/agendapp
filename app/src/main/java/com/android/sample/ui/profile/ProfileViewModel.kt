10bd0148b49018b2d529e093412c98c4ea297e9d
Poketimi <timael@andrie.ch>
2025-10-26 19:57:51 +0100

    fix : used the authRepository instead of hardcoded value

--- app/src/main/java/com/android/sample/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/android/sample/ui/profile/ProfileViewModel.kt
@@ -2,6 +2,7 @@ package com.android.sample.ui.profile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.android.sample.model.authentification.AuthRepositoryProvider
 import com.github.se.bootcamp.model.authentication.AuthRepository
 import com.github.se.bootcamp.model.authentication.AuthRepositoryFirebase
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -27,7 +28,7 @@ data class ProfileUIState(
  *
  * @property repository The repository used to retrieve user information.
  */
-class ProfileViewModel(private val repository: AuthRepository = AuthRepositoryFirebase()) :
+class ProfileViewModel(private val repository: AuthRepository = AuthRepositoryProvider.repository) :
     ViewModel() {
 
   private val _uiState = MutableStateFlow(ProfileUIState())

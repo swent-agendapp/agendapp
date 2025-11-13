5f3c2e521075b3589dfd932bb5b4c5b9bf6aeb5b
Qookie <nathan.duchosal@epfl.ch>
2025-11-14 00:18:58 +0100

    fix: reformat code

--- app/src/main/java/com/android/sample/ui/authentification/SignInScreen.kt
+++ app/src/main/java/com/android/sample/ui/authentification/SignInScreen.kt
@@ -1,20 +1,13 @@
 package com.github.se.bootcamp.ui.authentication
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -29,7 +22,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
@@ -140,9 +132,8 @@ fun SignInScreen(
 @Composable
 fun GoogleSignInButton(onSignInClick: () -> Unit) {
   PrimaryButton(
-    modifier =
-      Modifier.testTag(SignInScreenTestTags.LOGIN_BUTTON),
-    text = stringResource(R.string.sign_in_button_text),
-    onClick = onSignInClick,
-    )
+      modifier = Modifier.testTag(SignInScreenTestTags.LOGIN_BUTTON),
+      text = stringResource(R.string.sign_in_button_text),
+      onClick = onSignInClick,
+  )
 }

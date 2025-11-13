856a3ca327c241c42c6cca60bfaf8d9f3c62a203
Poketimi <164884349+Poketimi@users.noreply.github.com>
2025-11-13 20:25:41 +0100

    Add aliases for authentication package typo

---
+++ app/src/main/java/com/android/sample/model/authentification/AuthenticationAliases.kt
@@ -0,0 +1,12 @@
+package com.android.sample.model.authentification
+
+/**
+ * Backwards-compatible aliases that expose the authentication-domain types from the
+ * misspelled `authentification` package.
+ */
+typealias User = com.android.sample.model.authentication.User
+
+typealias UserRepository = com.android.sample.model.authentication.UserRepository
+
+typealias FirebaseUserRepository =
+    com.android.sample.model.authentication.FirebaseUserRepository

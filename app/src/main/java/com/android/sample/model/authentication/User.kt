761547d2a28cfff541845f713512a2102c8d2427
noafl-o <noafloret@gmail.com>
2025-11-18 19:13:01 +0100

    refactor: update User data class to generate UUID by default and simplify Employee initialization

--- app/src/main/java/com/android/sample/model/authentication/User.kt
+++ app/src/main/java/com/android/sample/model/authentication/User.kt
@@ -1,15 +1,17 @@
 package com.android.sample.model.authentication
 
+import java.util.UUID
+
 /**
  * Data class representing a User.
  *
- * @property id Unique identifier for the User.
+ * @property id Unique identifier for the User, generated as a UUID string by default.
  * @property displayName Name to display for User Profile.
  * @property email Email address of the User.
  * @property phoneNumber Phone number of the User.
  */
 data class User(
-    val id: String,
+    val id: String = UUID.randomUUID().toString(),
     val displayName: String?,
     val email: String?,
     val phoneNumber: String? = null,
--- app/src/main/java/com/android/sample/model/organization/Employee.kt
+++ app/src/main/java/com/android/sample/model/organization/Employee.kt
@@ -3,6 +3,6 @@ package com.android.sample.model.organization
 import com.android.sample.model.authentication.User
 
 data class Employee(
-    val user: User = User("", "", "", ""),
+    val user: User = User(displayName = "", email = ""),
     val role: Role = Role.EMPLOYEE,
 )

2f60cf2e1c3e063e4744fcb0cb959b8edcd36cbe
Qookie <nathan.duchosal@epfl.ch>
2025-11-27 23:20:15 +0100

    feat: add repository for Location

---
+++ app/src/main/java/com/android/sample/model/map/LocationRepository.kt
@@ -0,0 +1,38 @@
+package com.android.sample.model.map
+
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository interface for location-related operations.
+ *
+ * Provides operations to fetch user location and perform location-based checks.
+ */
+interface LocationRepository {
+
+  /**
+   * Fetches the user's current location.
+   *
+   * This method will attempt to get the user's location using the device's location services. It
+   * first tries to get the last known location for efficiency. If no cached location is available,
+   * it requests a fresh location from the GPS.
+   *
+   * @return The user's current Location.
+   * @throws SecurityException if location permissions are not granted.
+   * @throws Exception if location cannot be fetched.
+   */
+  suspend fun getUserLocation(): Location
+
+  /**
+   * Fetches the user's current location and checks if it is inside any of the provided areas.
+   *
+   * This method will attempt to get the user's location using the device's location services. It
+   * first tries to get the last known location for efficiency. If no cached location is available,
+   * it requests a fresh location from the GPS.
+   *
+   * @param areas List of Area objects to check against the user's location.
+   * @return true if the user's location is inside any of the provided areas, false otherwise.
+   * @throws SecurityException if location permissions are not granted.
+   * @throws Exception if location cannot be fetched or other errors occur.
+   */
+  suspend fun isUserLocationInAreas(areas: List<Area>): Boolean
+}

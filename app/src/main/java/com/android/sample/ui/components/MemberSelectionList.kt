5ba9a028f6ee5bf0551618812f941f3bc88277a7
Emilien Barde <emilien.barde@epfl.ch>
2025-12-02 14:33:18 +0100

    fix: improve MemberSelectionList design

--- app/src/main/java/com/android/sample/ui/components/MemberSelectionList.kt
+++ app/src/main/java/com/android/sample/ui/components/MemberSelectionList.kt
@@ -2,6 +2,7 @@ package com.android.sample.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,15 +11,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -28,21 +30,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import com.android.sample.R
-import com.android.sample.ui.theme.CircusPalette
 import com.android.sample.ui.theme.CornerRadiusLarge
+import com.android.sample.ui.theme.DefaultCardElevation
+import com.android.sample.ui.theme.GeneralPalette
 import com.android.sample.ui.theme.PaddingMedium
+import com.android.sample.ui.theme.WeightFadeEffect
 import com.android.sample.ui.theme.WeightVeryHeavy
 
 // the complexity was reduced with the help of IA
 data class MemberSelectionListOptions(
     val isSingleSelection: Boolean = false,
-    val highlightColor: Color = CircusPalette.Primary.copy(alpha = 0.9f),
+    val highlightColor: Color = GeneralPalette.Secondary.copy(alpha = 0.9f),
     val searchTestTag: String? = null,
     val listTestTag: String? = null,
     val summaryTestTag: String? = null,
@@ -65,25 +71,30 @@ fun MemberSelectionList(
         members.filter { it.contains(searchQuery, ignoreCase = true) }
       }
 
-  Column(modifier = modifier.fillMaxSize()) {
-    MemberSearchBar(
-        searchQuery = searchQuery,
-        onSearchQueryChange = { searchQuery = it },
-        options = options,
-    )
+  Card(
+      modifier = modifier.fillMaxWidth(),
+      elevation = CardDefaults.cardElevation(defaultElevation = DefaultCardElevation),
+      shape = RoundedCornerShape(CornerRadiusLarge)) {
+        Column(modifier = modifier.fillMaxSize()) {
+          MemberSearchBar(
+              searchQuery = searchQuery,
+              onSearchQueryChange = { searchQuery = it },
+              options = options,
+          )
 
-    MemberSelectionLazyList(
-        members = filteredMembers,
-        selectedMembers = selectedMembers,
-        onSelectionChanged = onSelectionChanged,
-        options = options,
-        modifier = Modifier.weight(WeightVeryHeavy))
+          MemberSelectionLazyList(
+              members = filteredMembers,
+              selectedMembers = selectedMembers,
+              onSelectionChanged = onSelectionChanged,
+              options = options,
+              modifier = Modifier.weight(WeightVeryHeavy))
 
-    MemberSelectionSummary(
-        selectedMembers = selectedMembers,
-        summaryTestTag = options.summaryTestTag,
-    )
-  }
+          MemberSelectionSummary(
+              selectedMembers = selectedMembers,
+              summaryTestTag = options.summaryTestTag,
+          )
+        }
+      }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -149,8 +160,11 @@ private fun MemberSelectionLazyList(
                           base
                         }
                       },
-              contentAlignment = Alignment.Center) {
-                Text(text = member, textAlign = TextAlign.Center)
+              contentAlignment = Alignment.CenterStart) {
+                Text(
+                    text = member,
+                    modifier = modifier.padding(start = PaddingMedium),
+                    textAlign = TextAlign.Center)
               }
 
           HorizontalDivider(
@@ -174,28 +188,51 @@ private fun MemberSelectionSummary(
       } else {
         pluralStringResource(
             R.plurals.replacement_selected_members,
-            selectedMembers.size,
-            selectedMembers.joinToString(", "),
+            count = selectedMembers.size,
+            selectedMembers.joinToString(
+                separator = ", ",
+                // extra space as a postfix to be able to scroll enough and not see the fade effect
+                // at the end of the last name
+                postfix = "      "),
         )
       }
 
-  OutlinedTextField(
-      value = selectedMembersText,
-      onValueChange = {},
+  var boxWidth by remember { mutableStateOf(0) } // used to adapt Fade start
+
+  Box(
       modifier =
-          modifier.fillMaxWidth().let { base ->
-            if (summaryTestTag != null) base.testTag(summaryTestTag) else base
-          },
-      singleLine = true,
-      shape = RoundedCornerShape(CornerRadiusLarge),
-      readOnly = true,
-      colors =
-          OutlinedTextFieldDefaults.colors(
-              focusedBorderColor = Color.Transparent,
-              unfocusedBorderColor = Color.Transparent,
-              disabledBorderColor = Color.Transparent,
-          ),
-  )
+          modifier
+              .fillMaxWidth()
+              .background(
+                  color = CardDefaults.cardColors().containerColor,
+                  shape = RoundedCornerShape(CornerRadiusLarge))
+              .padding(horizontal = PaddingMedium, vertical = PaddingMedium)
+              .onSizeChanged { boxWidth = it.width }) {
+        val scrollState = rememberScrollState()
+
+        // Scrollable text
+        Text(
+            text = selectedMembersText,
+            maxLines = 1,
+            modifier =
+                Modifier.horizontalScroll(scrollState).align(Alignment.CenterStart).let { base ->
+                  if (summaryTestTag != null) base.testTag(summaryTestTag) else base
+                },
+        )
+
+        // Fade on the right of the Text
+        Box(
+            modifier =
+                Modifier.matchParentSize()
+                    .background(
+                        brush =
+                            Brush.horizontalGradient(
+                                colors =
+                                    listOf(
+                                        Color.Transparent,
+                                        CardDefaults.cardColors().containerColor),
+                                startX = boxWidth * WeightFadeEffect)))
+      }
 }
 
 private fun calculateNewSelection(

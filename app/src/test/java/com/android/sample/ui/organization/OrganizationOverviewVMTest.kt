91f93a84be44d7267d3fc245192afc286ffb3221
Qookie <nathan.duchosal@epfl.ch>
2025-12-05 13:31:27 +0100

    fix: change user to userid in test

--- app/src/test/java/com/android/sample/ui/organization/OrganizationOverviewVMTest.kt
+++ app/src/test/java/com/android/sample/ui/organization/OrganizationOverviewVMTest.kt
@@ -43,7 +43,7 @@ class OrganizationOverviewViewModelTest {
   fun `fillSelectedOrganizationDetails fills UI state correctly`() = runTest {
     val orgId = "org1"
     val org =
-        Organization(id = orgId, name = "My Organization", members = listOf(user1, user2, user3))
+        Organization(id = orgId, name = "My Organization", members = listOf(user1.id, user2.id, user3.id))
     organizationRepository.insertOrganization(org)
 
     vm.fillSelectedOrganizationDetails(orgId)
@@ -88,7 +88,7 @@ class OrganizationOverviewViewModelTest {
   @Test
   fun `deleteSelectedOrganization removes org and clears selection`() = runTest {
     val orgId = "orgToDelete"
-    val org = Organization(id = orgId, name = "Deletable Org", members = listOf(user1))
+    val org = Organization(id = orgId, name = "Deletable Org", members = listOf(user1.id))
     organizationRepository.insertOrganization(org)
 
     vm.deleteSelectedOrganization(orgId)

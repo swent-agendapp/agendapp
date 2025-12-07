ce9cae8017819c2d3e7d28b87ecac264a24fb061
noafl-o <noafloret@gmail.com>
2025-12-07 22:56:43 +0100

    feat: add RequiresSelectedOrganizationTest interface for tests the need a selectedOrganization

---
+++ app/src/androidTest/java/com/android/sample/utils/RequiresSelectedOrganizationTest.kt
@@ -0,0 +1,26 @@
+package com.android.sample.utils
+
+import com.android.sample.ui.organization.SelectedOrganizationVMProvider
+
+/**
+ * An interface to be implemented by tests that require a selected organization.
+ *
+ * This interface provides a mechanism to set a specific organization as selected before each test
+ * is run.
+ */
+interface RequiresSelectedOrganizationTest {
+
+  /** The ID of the organization to be selected for the test. */
+  val organizationId: String
+
+  /**
+   * Prepares the selected organization before each test.
+   *
+   * Call this method in a @Before annotated function (e.g., in the setUp() method) of the test
+   * class.
+   */
+  fun setSelectedOrganization() {
+    val viewModel = SelectedOrganizationVMProvider.viewModel
+    viewModel.selectOrganization(organizationId)
+  }
+}

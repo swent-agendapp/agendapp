d58ee528accb8bd9eeb4066d736a2c1d6d265c13
noafl-o <noafloret@gmail.com>
2025-12-08 20:30:32 +0100

    feat: update EventRepositoryProvider to use hybrid EventRepository implementation

--- app/src/main/java/com/android/sample/data/global/providers/EventRepositoryProvider.kt
+++ app/src/main/java/com/android/sample/data/global/providers/EventRepositoryProvider.kt
@@ -1,20 +1,14 @@
 package com.android.sample.data.global.providers
 
-import com.android.sample.data.firebase.repositories.EventRepositoryFirebase
 import com.android.sample.data.global.repositories.EventRepository
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
+import com.android.sample.data.hybrid.repositories.EventRepositoryHybrid
 
 /**
  * Provides a single instance of the repository in the app. `repository` is mutable for testing
  * purposes.
  */
 object EventRepositoryProvider {
-  private val _repository: EventRepository by lazy {
-    // Change this to switch between different implementations
-    // EventRepositoryLocal()
-    EventRepositoryFirebase(Firebase.firestore)
-  }
+  private val _repository: EventRepository by lazy { EventRepositoryHybrid() }
 
   var repository: EventRepository = _repository
 }

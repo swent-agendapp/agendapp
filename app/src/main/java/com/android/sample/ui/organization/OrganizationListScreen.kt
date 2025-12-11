ab45f64262c78e543c995150758f4b7395072b84
N4dZ <dingweifeng.dw@gmail.com>
2025-12-11 01:22:12 +0100

    feat: add an entry point of the use invitation to join feature in OrganizationListScreen.kt

--- app/src/main/java/com/android/sample/ui/organization/OrganizationListScreen.kt
+++ app/src/main/java/com/android/sample/ui/organization/OrganizationListScreen.kt
@@ -1,26 +1,33 @@
 package com.android.sample.ui.organization
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Login
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Business
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
@@ -32,8 +39,10 @@ import com.android.sample.ui.common.FloatingButton
 import com.android.sample.ui.common.Loading
 import com.android.sample.ui.common.MainPageButton
 import com.android.sample.ui.common.MainPageTopBar
+import com.android.sample.ui.invitation.useInvitation.UseInvitationBottomSheet
 import com.android.sample.ui.theme.PaddingMedium
 import com.android.sample.ui.theme.SpacingMedium
+import kotlinx.coroutines.launch
 
 object OrganizationListScreenTestTags {
   const val ROOT = "organizationListScreenRoot"
@@ -48,6 +57,7 @@ object OrganizationListScreenTestTags {
   const val SNACK_BAR = "snackBar"
 
   const val PULL_TO_REFRESH = "pullToRefresh"
+  const val USE_INVITATION_BOTTOM_SHEET = "useInvitationBottomSheet"
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -58,7 +68,10 @@ fun OrganizationListScreen(
         SelectedOrganizationVMProvider.viewModel,
     onOrganizationSelected: () -> Unit = {},
     onAddOrganizationClicked: () -> Unit = {},
+    onUseInvitationClicked: () -> Unit = {}
 ) {
+  val sheetState = rememberModalBottomSheetState()
+  val scope = rememberCoroutineScope()
   val uiState by organizationViewModel.uiState.collectAsState()
   val snackBarHostState = remember { SnackbarHostState() }
 
@@ -77,17 +90,32 @@ fun OrganizationListScreen(
         )
       },
       floatingActionButton = {
-        FloatingButton(
-            onClick = onAddOrganizationClicked,
-            icon = Icons.Default.Add,
-            modifier = Modifier.testTag(OrganizationListScreenTestTags.ADD_ORGANIZATION_BUTTON))
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(end = PaddingMedium, start = PaddingMedium),
+            horizontalArrangement = Arrangement.SpaceBetween) {
+              FloatingButton(
+                  onClick = {
+                    scope.launch { sheetState.show() }
+                    organizationViewModel.setShowUseInvitationBottomSheet(true)
+                    onUseInvitationClicked
+                  },
+                  icon = Icons.AutoMirrored.Filled.Login,
+              )
+
+              FloatingButton(
+                  onClick = onAddOrganizationClicked,
+                  icon = Icons.Default.Add,
+                  modifier =
+                      Modifier.testTag(OrganizationListScreenTestTags.ADD_ORGANIZATION_BUTTON))
+            }
       },
       modifier = Modifier.testTag(OrganizationListScreenTestTags.ROOT),
       snackbarHost = {
         SnackbarHost(
             hostState = snackBarHostState,
             modifier = Modifier.testTag(OrganizationListScreenTestTags.SNACK_BAR))
-      }) { innerPadding ->
+      },
+      content = { innerPadding ->
         PullToRefreshBox(
             isRefreshing = uiState.isRefreshing,
             onRefresh = { organizationViewModel.refreshOrganizations() },
@@ -120,7 +148,23 @@ fun OrganizationListScreen(
                     }
                   }
             }
-      }
+      })
+  if (uiState.showUseInvitationBottomSheet) {
+    ModalBottomSheet(
+        modifier = Modifier.testTag(OrganizationListScreenTestTags.USE_INVITATION_BOTTOM_SHEET),
+        onDismissRequest = { organizationViewModel.setShowUseInvitationBottomSheet(false) },
+        sheetState = sheetState) {
+          UseInvitationBottomSheet(
+              onCancel = {
+                scope.launch { sheetState.hide() }
+                organizationViewModel.setShowUseInvitationBottomSheet(false)
+              },
+              onJoin = {
+                scope.launch { sheetState.hide() }
+                organizationViewModel.setShowUseInvitationBottomSheet(false)
+              })
+        }
+  }
 }
 
 @Composable

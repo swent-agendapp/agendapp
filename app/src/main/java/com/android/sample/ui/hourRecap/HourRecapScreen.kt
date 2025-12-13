29a6c390bae3861e310873391fdd048d18591ec0
Poketimi <164884349+Poketimi@users.noreply.github.com>
2025-12-13 08:01:58 +0100

    feat : added colors to cards

--- app/src/main/java/com/android/sample/ui/hourRecap/HourRecapScreen.kt
+++ app/src/main/java/com/android/sample/ui/hourRecap/HourRecapScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.platform.LocalContext
@@ -340,7 +341,7 @@ private fun HourRecapEventCard(
             text = event.title,
             style = MaterialTheme.typography.titleSmall,
             fontWeight = FontWeight.SemiBold)
-        Spacer(Modifier.height(PaddingExtraSmall))
+        Spacer(Modifier.height(PaddingSmall))
         Text(
             text =
                 stringResource(

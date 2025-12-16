eb29e2a65a05226d164b814b5626b40c73d9d845
nillem-14 <meline@rmbi.ch>
2025-12-16 17:57:31 +0100

    fix: adapt test to new behavior

--- app/src/androidTest/java/com/android/sample/ui/addEvent/AddEventTitleBottomBarTest.kt
+++ app/src/androidTest/java/com/android/sample/ui/addEvent/AddEventTitleBottomBarTest.kt
@@ -5,22 +5,27 @@ import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import com.android.sample.ui.calendar.addEvent.AddEventTestTags
+import com.android.sample.ui.calendar.addEvent.AddEventViewModel
 import com.android.sample.ui.calendar.addEvent.components.AddEventTitleAndDescriptionBottomBar
+import com.android.sample.utils.FirebaseEmulatedTest
 import com.android.sample.utils.RequiresSelectedOrganizationTestBase
 import com.android.sample.utils.RequiresSelectedOrganizationTestBase.Companion.DEFAULT_TEST_ORG_ID
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-class AddEventTitleBottomBarTest : RequiresSelectedOrganizationTestBase {
+class AddEventTitleBottomBarTest : FirebaseEmulatedTest(), RequiresSelectedOrganizationTestBase {
 
   override val organizationId: String = DEFAULT_TEST_ORG_ID
+  private lateinit var addEventViewModel: AddEventViewModel
 
   @get:Rule val composeTestRule = createComposeRule()
 
   @Before
-  fun setUp() {
+  override fun setUp() {
+    super.setUp()
     setSelectedOrganization()
+    addEventViewModel = AddEventViewModel()
 
     composeTestRule.setContent { AddEventTitleAndDescriptionBottomBar() }
   }

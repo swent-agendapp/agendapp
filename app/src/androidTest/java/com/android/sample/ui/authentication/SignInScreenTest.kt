eb9670f097c27e5ec6b3783c9e9c678067231613
noafl-o <noafloret@gmail.com>
2025-12-16 21:01:07 +0100

    feat: add network availability handling to SignInScreen and SignInViewModel

--- app/src/androidTest/java/com/android/sample/ui/authentication/SignInScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/ui/authentication/SignInScreenTest.kt
@@ -2,26 +2,36 @@ package com.android.sample.ui.authentication
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.sample.R
+import com.android.sample.model.network.FakeConnectivityChecker
+import com.android.sample.model.network.NetworkStatusRepository
 import com.android.sample.utils.FirebaseEmulator
+import com.android.sample.utils.NetworkTestBase
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-class SignInScreenTest {
+class SignInScreenTest : NetworkTestBase {
   @get:Rule val composeTestRule = createComposeRule()
+
+  override val fakeChecker = FakeConnectivityChecker(state = true)
+  override val networkRepo = NetworkStatusRepository(fakeChecker)
 
   @Before
   fun setUp() {
+    setupNetworkTestBase()
     FirebaseEmulator.auth.signOut()
+
+    composeTestRule.setContent { SignInScreen() }
   }
 
   @Test
   fun topTitleIsCorrectlySet() {
-    composeTestRule.setContent { SignInScreen() }
-
     composeTestRule.onNodeWithTag(SignInScreenTestTags.APP_LOGO).assertIsDisplayed()
     composeTestRule
         .onNodeWithTag(SignInScreenTestTags.LOGIN_TITLE)
@@ -37,5 +47,24 @@ class SignInScreenTest {
         .assertTextEquals("Welcome")
     composeTestRule.onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON).assertIsDisplayed()
     composeTestRule.onNodeWithTag(SignInScreenTestTags.LOGOUT_BUTTON).assertIsNotDisplayed()
+  }
+
+  @Test
+  fun signInButtonDisabledWhenNoNetwork() {
+    simulateNoInternet()
+
+    // Check the button is displayed and disabled
+    composeTestRule
+        .onNodeWithTag(SignInScreenTestTags.LOGIN_BUTTON)
+        .assertIsDisplayed()
+        .assertIsNotEnabled()
+
+    val context = InstrumentationRegistry.getInstrumentation().targetContext
+    val expectedText = context.getString(R.string.network_error_message)
+
+    // Check the text message is correct
+    composeTestRule
+        .onNodeWithTag(SignInScreenTestTags.NO_NETWORK_MESSAGE)
+        .assertTextEquals(expectedText)
   }
 }

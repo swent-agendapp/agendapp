3189b600a2775de1d84a4f4f13f2ab367e5c8196
noafl-o <noafloret@gmail.com>
2025-12-17 20:59:42 +0100

    refactor: lower OrganizationListScreen complexity

--- app/src/main/java/com/android/sample/ui/organization/OrganizationListScreen.kt
+++ app/src/main/java/com/android/sample/ui/organization/OrganizationListScreen.kt
@@ -3,6 +3,7 @@ package com.android.sample.ui.organization
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,6 +18,7 @@ import androidx.compose.material.icons.filled.Business
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -43,10 +45,12 @@ import com.android.sample.ui.common.Loading
 import com.android.sample.ui.common.MainPageButton
 import com.android.sample.ui.common.MainPageTopBar
 import com.android.sample.ui.invitation.useInvitation.UseInvitationBottomSheet
+import com.android.sample.ui.invitation.useInvitation.UseInvitationUIState
 import com.android.sample.ui.invitation.useInvitation.UseInvitationViewModel
 import com.android.sample.ui.theme.PaddingExtraSmall
 import com.android.sample.ui.theme.PaddingMedium
 import com.android.sample.ui.theme.SpacingMedium
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 object OrganizationListScreenTestTags {
@@ -81,146 +85,200 @@ fun OrganizationListScreen(
   val organizationUIState by organizationViewModel.uiState.collectAsState()
   val useInvitationUIState by useInvitationViewModel.uiState.collectAsState()
   val snackBarHostState = remember { SnackbarHostState() }
-
-  // To trigger a Snackbar if the disabled button is clicked
   var disabledButtonClicked by remember { mutableStateOf(false) }
 
   val noNetworkErrorMsg = stringResource(R.string.network_error_message)
-  // Observe the disabled button click and show Snackbar
+  handleErrors(
+      disabledButtonClicked,
+      onConsumed = { disabledButtonClicked = false },
+      snackBarHostState = snackBarHostState,
+      organizationUIState = organizationUIState,
+      useInvitationUIState = useInvitationUIState,
+      useInvitationViewModel = useInvitationViewModel)
+
+  LaunchedEffect(Unit) { organizationViewModel.loadOrganizations() }
+
+  Scaffold(
+      topBar = { MainPageTopBar(title = stringResource(R.string.organization_list_title)) },
+      floatingActionButton = {
+        FloatingButtonsSection(
+            scope,
+            sheetState,
+            organizationUIState,
+            onAddOrganizationClicked,
+            organizationViewModel,
+            disabledButtonClickedSetter = { disabledButtonClicked = it })
+      },
+      snackbarHost = {
+        SnackbarHost(
+            hostState = snackBarHostState,
+            modifier = Modifier.testTag(OrganizationListScreenTestTags.SNACK_BAR))
+      },
+      modifier = Modifier.testTag(OrganizationListScreenTestTags.ROOT),
+      content = { innerPadding ->
+        OrganizationListContent(
+            organizationUIState = organizationUIState,
+            useInvitationUIState = useInvitationUIState,
+            selectedOrganizationViewModel = selectedOrganizationViewModel,
+            onOrganizationSelected = onOrganizationSelected,
+            innerPadding = innerPadding,
+            disabledButtonClickedSetter = { disabledButtonClicked = it })
+      })
+
+  if (organizationUIState.showUseInvitationBottomSheet) {
+    UseInvitationModal(sheetState, useInvitationViewModel, organizationViewModel, scope)
+  }
+}
+
+@Composable
+private fun handleErrors(
+    disabledButtonClicked: Boolean,
+    onConsumed: () -> Unit,
+    snackBarHostState: SnackbarHostState,
+    organizationUIState: OrganizationUIState,
+    useInvitationUIState: UseInvitationUIState,
+    useInvitationViewModel: UseInvitationViewModel
+) {
+  val noNetworkErrorMsg = stringResource(R.string.network_error_message)
+
   LaunchedEffect(disabledButtonClicked) {
     if (disabledButtonClicked) {
       snackBarHostState.showSnackbar(noNetworkErrorMsg)
-      disabledButtonClicked = false
+      onConsumed()
     }
   }
 
-  val useInvitationErrMsg =
-      useInvitationUIState.errorMessageId?.let {
-        stringResource(id = useInvitationUIState.errorMessageId!!)
-      }
+  val useInvitationErrMsg = useInvitationUIState.errorMessageId?.let { stringResource(it) }
+
   LaunchedEffect(useInvitationErrMsg) {
-    useInvitationErrMsg?.let { errorMessage ->
-      snackBarHostState.showSnackbar(errorMessage)
+    useInvitationErrMsg?.let {
+      snackBarHostState.showSnackbar(it)
       useInvitationViewModel.setError(null)
     }
   }
 
   LaunchedEffect(organizationUIState.errorMsg, useInvitationErrMsg) {
     organizationUIState.errorMsg?.let {
-      snackBarHostState.showSnackbar(
-          message = organizationUIState.errorMsg.toString(), duration = SnackbarDuration.Short)
-      organizationViewModel.clearErrorMsg()
+      snackBarHostState.showSnackbar(it, duration = SnackbarDuration.Short)
+      useInvitationViewModel.setError(null)
     }
   }
-  LaunchedEffect(Unit) { organizationViewModel.loadOrganizations() }
+}
 
-  Scaffold(
-      topBar = {
-        MainPageTopBar(
-            title = stringResource(R.string.organization_list_title),
-        )
-      },
-      floatingActionButton = {
-        Box(
-            modifier =
-                Modifier.fillMaxWidth()
-                    .padding(end = PaddingExtraSmall, bottom = PaddingExtraSmall)) {
-              Column(
-                  modifier = Modifier.align(Alignment.BottomEnd),
-                  verticalArrangement = Arrangement.spacedBy(PaddingMedium)) {
-                    FloatingButton(
-                        onClick = {
-                          if (organizationUIState.networkAvailable) {
-                            scope.launch { sheetState.show() }
-                            organizationViewModel.setShowUseInvitationBottomSheet(true)
-                          } else {
-                            disabledButtonClicked = true
-                          }
-                        },
-                        icon = Icons.AutoMirrored.Filled.Login,
-                        modifier =
-                            Modifier.testTag(OrganizationListScreenTestTags.USE_INVITATION_BUTTON),
-                        enabled = organizationUIState.networkAvailable)
-                    FloatingButton(
-                        onClick = {
-                          if (organizationUIState.networkAvailable) {
-                            onAddOrganizationClicked()
-                          } else {
-                            disabledButtonClicked = true
-                          }
-                        },
-                        icon = Icons.Default.Add,
-                        modifier =
-                            Modifier.testTag(
-                                OrganizationListScreenTestTags.ADD_ORGANIZATION_BUTTON),
-                        enabled = organizationUIState.networkAvailable,
-                    )
-                  }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun FloatingButtonsSection(
+    scope: CoroutineScope,
+    sheetState: SheetState,
+    organizationUIState: OrganizationUIState,
+    onAddOrganizationClicked: () -> Unit,
+    organizationViewModel: OrganizationViewModel,
+    disabledButtonClickedSetter: (Boolean) -> Unit
+) {
+  Box(
+      modifier =
+          Modifier.fillMaxWidth().padding(end = PaddingExtraSmall, bottom = PaddingExtraSmall)) {
+        Column(
+            modifier = Modifier.align(Alignment.BottomEnd),
+            verticalArrangement = Arrangement.spacedBy(PaddingMedium)) {
+              FloatingButton(
+                  onClick = {
+                    if (organizationUIState.networkAvailable) {
+                      scope.launch { sheetState.show() }
+                      organizationViewModel.setShowUseInvitationBottomSheet(true)
+                    } else {
+                      disabledButtonClickedSetter(true)
+                    }
+                  },
+                  icon = Icons.AutoMirrored.Filled.Login,
+                  modifier = Modifier.testTag(OrganizationListScreenTestTags.USE_INVITATION_BUTTON),
+                  enabled = organizationUIState.networkAvailable)
+              FloatingButton(
+                  onClick = {
+                    if (organizationUIState.networkAvailable) {
+                      onAddOrganizationClicked()
+                    } else {
+                      disabledButtonClickedSetter(true)
+                    }
+                  },
+                  icon = Icons.Default.Add,
+                  modifier =
+                      Modifier.testTag(OrganizationListScreenTestTags.ADD_ORGANIZATION_BUTTON),
+                  enabled = organizationUIState.networkAvailable)
             }
-      },
-      modifier = Modifier.testTag(OrganizationListScreenTestTags.ROOT),
-      snackbarHost = {
-        SnackbarHost(
-            hostState = snackBarHostState,
-            modifier = Modifier.testTag(OrganizationListScreenTestTags.SNACK_BAR))
-      },
-      content = { innerPadding ->
-        PullToRefreshBox(
-            isRefreshing = organizationUIState.isRefreshing,
-            onRefresh = { organizationViewModel.refreshOrganizations() },
+      }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun OrganizationListContent(
+    organizationUIState: OrganizationUIState,
+    useInvitationUIState: UseInvitationUIState,
+    selectedOrganizationViewModel: SelectedOrganizationViewModel,
+    onOrganizationSelected: () -> Unit,
+    innerPadding: PaddingValues,
+    disabledButtonClickedSetter: (Boolean) -> Unit
+) {
+  PullToRefreshBox(
+      isRefreshing = organizationUIState.isRefreshing,
+      onRefresh = { /* call refresh */},
+      modifier =
+          Modifier.fillMaxSize()
+              .padding(innerPadding)
+              .testTag(OrganizationListScreenTestTags.PULL_TO_REFRESH)) {
+        Column(
             modifier =
                 Modifier.fillMaxSize()
-                    .padding(innerPadding)
-                    .testTag(OrganizationListScreenTestTags.PULL_TO_REFRESH)) {
-              Column(
-                  modifier =
-                      Modifier.fillMaxSize()
-                          .verticalScroll(rememberScrollState())
-                          .padding(PaddingMedium)) {
-                    if (organizationUIState.isLoading || useInvitationUIState.isTemptingToJoin) {
-                      Loading(
-                          label =
-                              if (organizationUIState.isLoading)
-                                  stringResource(R.string.organization_loading)
-                              else stringResource(R.string.invitation_joining_organization),
-                          modifier =
-                              Modifier.fillMaxSize()
-                                  .testTag(OrganizationListScreenTestTags.LOADING_INDICATOR))
-                    } else {
-                      Spacer(modifier = Modifier.height(SpacingMedium))
-                      OrganizationList(
-                          organizations = organizationUIState.organizations,
-                          onOrganizationSelected = { organization ->
-                            if (organizationUIState.networkAvailable) {
-                              selectedOrganizationViewModel.selectOrganization(
-                                  orgId = organization.id)
-                              onOrganizationSelected()
-                            } else {
-                              disabledButtonClicked = true
-                            }
-                          },
-                          enabled = organizationUIState.networkAvailable)
-                    }
-                  }
+                    .verticalScroll(rememberScrollState())
+                    .padding(PaddingMedium)) {
+              if (organizationUIState.isLoading || useInvitationUIState.isTemptingToJoin) {
+                Loading(
+                    label =
+                        if (organizationUIState.isLoading)
+                            stringResource(R.string.organization_loading)
+                        else stringResource(R.string.invitation_joining_organization),
+                    modifier = Modifier.testTag(OrganizationListScreenTestTags.LOADING_INDICATOR))
+              } else {
+                Spacer(modifier = Modifier.height(SpacingMedium))
+                OrganizationList(
+                    organizations = organizationUIState.organizations,
+                    onOrganizationSelected = { org ->
+                      if (organizationUIState.networkAvailable) {
+                        selectedOrganizationViewModel.selectOrganization(org.id)
+                        onOrganizationSelected()
+                      } else {
+                        disabledButtonClickedSetter(true)
+                      }
+                    },
+                    enabled = organizationUIState.networkAvailable)
+              }
             }
-      })
-  if (organizationUIState.showUseInvitationBottomSheet) {
-    ModalBottomSheet(
-        modifier = Modifier.testTag(OrganizationListScreenTestTags.USE_INVITATION_BOTTOM_SHEET),
-        onDismissRequest = { organizationViewModel.setShowUseInvitationBottomSheet(false) },
-        sheetState = sheetState) {
-          UseInvitationBottomSheet(
-              useInvitationViewModel = useInvitationViewModel,
-              onCancel = {
-                scope.launch { sheetState.hide() }
-                organizationViewModel.setShowUseInvitationBottomSheet(false)
-              },
-              onJoin = {
-                scope.launch { sheetState.hide() }
-                organizationViewModel.setShowUseInvitationBottomSheet(false)
-              })
-        }
-  }
+      }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun UseInvitationModal(
+    sheetState: SheetState,
+    useInvitationViewModel: UseInvitationViewModel,
+    organizationViewModel: OrganizationViewModel,
+    scope: CoroutineScope
+) {
+  ModalBottomSheet(
+      modifier = Modifier.testTag(OrganizationListScreenTestTags.USE_INVITATION_BOTTOM_SHEET),
+      onDismissRequest = { organizationViewModel.setShowUseInvitationBottomSheet(false) },
+      sheetState = sheetState) {
+        UseInvitationBottomSheet(
+            useInvitationViewModel = useInvitationViewModel,
+            onCancel = {
+              scope.launch { sheetState.hide() }
+              organizationViewModel.setShowUseInvitationBottomSheet(false)
+            },
+            onJoin = {
+              scope.launch { sheetState.hide() }
+              organizationViewModel.setShowUseInvitationBottomSheet(false)
+            })
+      }
 }
 
 @Composable
